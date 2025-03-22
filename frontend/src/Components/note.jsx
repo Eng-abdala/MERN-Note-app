cbf83a0649d76ec26a9c526d54dcff1c28994589
Abdullahi3538 <alimohammedmohamud20@gmail.com>
2025-03-22 13:38:30 +1100

    motion

--- frontend/src/Components/note.jsx
+++ frontend/src/Components/note.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import axios from "axios"
 import {toast,Toaster} from "react-hot-toast"
+import { motion } from "framer-motion"
 
 import { useNavigate } from "react-router-dom"
 
@@ -32,14 +33,38 @@ const Note = ()=>{
     return <>
     <div className="flex justify-center mt-[15%]">
 
-    <form className="w-[400px] p-10 shadow-2xl shadow-blue-300" action="">
+    <motion.form className="w-[400px] p-10 shadow-2xl shadow-blue-300" action=""
+    initial={{opacity:0, x:"-100vh"}}
+    animate={{opacity:1, x:0}}
+    transition={{duration:1}}
+    
+    
+    >
         <h1 className="font-bold text-[22px] text-blue-500">Note App</h1>
         <label htmlFor="">Task:</label> <br />
-        <input value={title}  onChange={(event)=> setTitle(event.target.value)} className="w-[300px] pl-2 h-[30px] mb-4 border-2 border-blue-400 " type="text" placeholder="Enter a task" /> <br />
+        < motion.input  value={title}  onChange={(event)=> setTitle(event.target.value)} className="w-[300px] pl-2 h-[30px] mb-4 border-2 border-blue-400 " type="text" placeholder="Enter a task"
+         initial={{ opacity: 0, y: -20 }}
+         animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5 }}
+        
+        
+        /> <br />
         <label htmlFor="">Description:</label> <br />
-        <input value={description}  onChange={(event)=> setDescription(event.target.value)} className="w-[300px] pl-2 h-[30px] mb-4 border-2 border-blue-400" type="text" placeholder="Enter a description" /> <br />
-        <button onClick={PostData} className="px-[20px] py-0.5 bg-blue-400 font-bold text-white ml-20 mt-4 rounded-md ">Add</button>
-    </form>
+        <motion.input value={description}  onChange={(event)=> setDescription(event.target.value)} className="w-[300px] pl-2 h-[30px] mb-4 border-2 border-blue-400" type="text" placeholder="Enter a description"
+        
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1 }}
+        
+        /> <br />
+        <motion.button onClick={PostData} className="px-[20px] py-0.5 bg-blue-400 font-bold text-white ml-20 mt-4 rounded-md "
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.5 }}
+        
+        
+        >Add</motion.button>
+    </motion.form>
     </div>
 
     <Toaster></Toaster>
